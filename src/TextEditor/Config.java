cf26dc918b41735b63026586a58f4c2cea89030e
Luca Monteverdi <lucamontee@gmail.com>
2023-11-18 00:25:04 +0100

    Improvements in config loading and objects handling

--- src/TextEditor/Config.java
+++ src/TextEditor/Config.java
@@ -3,32 +3,29 @@ package TextEditor;
 import java.io.InputStream;
 import java.util.Scanner;
 
-public class Config extends TextEditor {
+public class Config {
 
 	private final static String SEPARATOR = "="; 
 
 	protected static void Parse() {
 
 		String[] splitline;
+		InputStream input = TextEditor.class.getResourceAsStream("/config/strings.txt");
+		Scanner objscanner = new Scanner(input);
 
 		try {
-
-			InputStream input = TextEditor.class.getResourceAsStream("/config/strings.txt");
-			Scanner objscanner = new Scanner(input);
-
 			while(objscanner.hasNextLine()) {
 				splitline = objscanner.nextLine().split(SEPARATOR, -2);
-				strings.put(splitline[0].trim(), splitline[1].trim());
+				TextEditor.PutString(splitline[0].trim(), splitline[1].trim());
 			}
-
-			objscanner.close();
-
 		} catch (Exception exception) {
 			if(exception.getClass().getSimpleName().equals("FileNotFoundException")) {
 				System.out.println("File di configurazione non trovato!");
 			} else {
 				exception.printStackTrace();
 			}
+		} finally {
+			objscanner.close();
 		}
 	}
 }
